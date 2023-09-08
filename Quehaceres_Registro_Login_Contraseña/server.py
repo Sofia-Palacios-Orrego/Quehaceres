90abbea02c6ce1d6be51fc86a18c1b24907eb91d
Sofía Palacios <sofia.palacios.orrego@gmail.com>
2023-09-07 22:55:44 -0300

    Add files via upload

---
+++ Quehaceres_Registro_Login_Contraseña/server.py
@@ -0,0 +1,5 @@
+from app_quehaceres import app
+from app_quehaceres.controladores import controlador_quehaceres, controlador_usuarios
+
+if __name__ == "__main__":
+    app.run( debug = True, port = 5001 )
